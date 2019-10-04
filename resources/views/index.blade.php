8ce6145c84e2c82843af11abd22a593041f67218
Daniele Praino <daniele.praino@gmail.com>
2019-10-04 09:37:09 +0200

    initial setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'One Page Layout')
+@section('description', 'Layout Atoms Studio')
+@section('web design', 'web development', 'UI UX design', 'marketing', 'SEO' )
+
+@section('content')
+  <header>
+
+  </header>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" integrity="sha384-wxqG4glGB3nlqX0bi23nmgwCSjWIW13BdLUEYC4VIMehfbcro/ATkyDsF/AbIOVe" crossorigin="anonymous">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+  </head>
+  <body>
+    @include('partials._nav')
+    @yield('content')
+    @include('partials._footer')
+    <script src="{{ asset('js/app.js') }}"></script>
+  </body>
+</html>
